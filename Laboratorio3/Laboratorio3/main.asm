fa900b7890175ceeaffb344c13943909f608551f
Hector <mar223@uvg.edu.gt>
2025-02-18 00:19:26 -0600

    LAB3_COMPLETO

--- Laboratorio3/Laboratorio3/main.asm
+++ Laboratorio3/Laboratorio3/main.asm
@@ -10,8 +10,9 @@
 //ENCABEZADO
 //***************************************************************
 .include "M328PDEF.inc"
-.def BIN_COUNTER = R20
-
+.def BIN_COUNTER = R20					;Contador binario
+.def HEX_COUNTER = R21					;Contador hexadecimal
+.def TICK_COUNTER = R22					;Contador de ticks para contar 1000ms
 ; Tabla de Vectores
 .org 0x0000
     RJMP SET_UP							;Vector de reset: salta a la rutina de configuración
@@ -30,6 +31,8 @@
     LDI     R16, HIGH(RAMEND)
     OUT     SPH, R16
 
+//TABLA 7 SEG
+SEG_TABLE:.db 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F,	0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71  
 
 SET_UP:
     ;Configurar Prescaler: F_CPU = 1 MHz
@@ -38,17 +41,19 @@ SET_UP:
     LDI     R16, 0b00000100				;Configurar Prescaler a 16 F_cpu = 1MHz 
     STS     CLKPR, R16
 
-    ;Configuración de PORTB: PB0-PB3 como salidas (LEDs)
-    LDI     R16, 0b00001111				;Configura bits 0 a 3 como salidas
+	;Configuración de E/S para contador binario
+    LDI     R16, 0b00001111				;Configuración de PORTB: PB0-PB3 como salidas (LEDs)
     OUT     DDRB, R16
-
     ;Configuración de PORTC: Entrada para los pushbuttons
     LDI     R16, 0x00
     OUT     DDRC, R16					;Puerto C como entrada
-
     ;Activar pull-ups internos en PC2 y PC3
     LDI     R16, 0x0C					;0x0C = 0b00001100
     OUT     PORTC, R16
+
+	;Configuración de E/S para contador hexadecimal (display)
+	LDI		R16, 0b11111111						;Configura PORTD como salida
+	OUT		DDRD, R16
 
     ;Deshabilitar el módulo serial (apaga otros LEDs del Arduino)
     LDI     R16, 0x00
@@ -56,14 +61,15 @@ SET_UP:
     STS     UCSR0B, R16
     STS     UCSR0C, R16
 
-    ; Inicializar el contador (R20) a 0
+    ;Inicializar el contadores a 0
     LDI     BIN_COUNTER, 0
+	LDI		HEX_COUNTER, 0
+	LDI		TICK_COUNTER, 0
 
     ;Configurar interrupciones por cambio (Pin Change Interrupt) para PORTC:
     ;Habilitar el grupo de interrupciones PCINT1 (PCINT8 a PCINT14)
     LDI     R16, (1<<PCIE1)
     STS     PCICR, R16
-
     ;Habilitar la interrupción en PC2 y PC3 (que corresponden a PCINT10 y PCINT11)
     LDI     R16, (1<<PCINT10) | (1<<PCINT11)
     STS     PCMSK1, R16
@@ -73,20 +79,16 @@ SET_UP:
     OUT     TCCR0A, R16
 	LDI		R16, (1<<CS02)			;Prescaler 256
     OUT     TCCR0B, R16
-	;OCR0A = 77
-	LDI		R16, 77
+	LDI		R16, 77					;OCR0A = 77
 	OUT		OCR0A, R16
-	;Habilitar interrupción Compare Match A de timer0
-	LDI		R16, (1<<OCIE0A)
+	LDI		R16, (1<<OCIE0A)		;Habilitar interrupción Compare Match A de timer0
 	STS		TIMSK0, R16
 
-    ;Habilitar interrupciones globales
-    SEI
+    SEI								;;Habilitar interrupciones globales
 
 ;Bucle principal (se queda aquí esperando la ISR)
 MAIN_LOOP:
     rjmp MAIN_LOOP				;Bucle infinito
-
 
 
 ;ISR: Interrupción por cambio en PORTC (PCINT1)
@@ -112,6 +114,31 @@ PCINT1_ISR:
 	RETI
 
 TIMER0_COMPA_ISR:
+	;Contador de ticks para el contador hexadecimal
+	INC		TICK_COUNTER		;Incrementar contador de ticks
+	CPI		TICK_COUNTER, 50	;50*20ms = 1000ms (1 segundo)
+	BRNE	NO_SEC				;no ha pasado un segundo, salta a anti rebote
+	;Pasa 1 segundo
+    LDI		TICK_COUNTER, 0      ;Reiniciar TICK_COUNTER
+    INC		HEX_COUNTER          ;Incrementar HEX_COUNTER (de 0 a 15)
+    CPI		HEX_COUNTER, 16      ;Si HEX_COUNTER == 16 (0x10)
+    BRLO	UPDATE_DISPLAY       ;Si es menor, continúa
+    LDI		HEX_COUNTER, 0       ;De lo contrario, reinicia a 0
+
+
+UPDATE_DISPLAY:
+	;Cargar la dirección base de SEG_TABLE en el puntero Z
+	LDI		ZH, HIGH(SEG_TABLE<<1)
+    LDI		ZL, LOW(SEG_TABLE<<1)
+    ;Sumar el valor de HEX_COUNTER (R19) para obtener el desplazamiento
+    ADD		ZL, HEX_COUNTER						;Suma el offset en la parte baja
+    ;Leer el patrón de segmentos desde la tabla en R16
+    LPM		R16, Z
+    ;Enviar el patrón a PORTD para actualizar el display
+    OUT		PORTD, R16
+
+
+NO_SEC:
     IN      R16, PINC			;Leer estado del puerto
     ANDI    R16, 0x0C           ;Enmascarar para conservar solo PC2 y PC3 (ambos con pull-up, sin presionar = 1)
     ; Si ambos botones están liberados, R16 debe ser 0x0C.
